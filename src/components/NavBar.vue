2ade9e04e04a308c64b60c80b715c0071029ab32
Dusan Todorovic <dusantodorovic1989@gmail.com>
2018-12-04 19:00:07 +0100

    zadatak 1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-light">
+  <a class="navbar-brand" href="#">Movies</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    
+        <router-link class="nav-link" to="/cars">Post</router-link>
+
+        <!-- <router-link class="nav-link" to="/add">Add Cars</router-link>
+      
+        <a class="nav-link" href="#">Features</a>
+        
+        <a class="nav-link" href="#">Pricing</a>
+      
+        <a class="nav-link" href="#">Disabled</a> -->
+    
+  </div>
+</nav>
+</template>
+
