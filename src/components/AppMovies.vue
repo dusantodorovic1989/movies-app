457a6fcf70e1724c66d4c254a36a4aade7c5aa48
Dusan Todorovic <dusantodorovic1989@gmail.com>
2018-12-04 19:29:33 +0100

    zadatak 3

---
+++ src/components/AppMovies.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import Movies from '../services/Movies.js'
+export default {
+
+    data(){
+        return{
+        movies: []
+        }
+    },
+
+    beforeRouteEnter(to, from, next) {
+       moviesService.getAll()
+           .then(response => {
+               next(vm => {
+                   vm.movies = response.data;
+               })
+           });
+   },
+    
+}
+</script>
+
--- src/services/Movies.js
+++ src/services/Movies.js
@@ -4,9 +4,9 @@ class Movies {
     constructor(){
         axios.defaults.baseURL='http://localhost:3000/api/'
     }
-    // getAll(){
-    //     return axios.get('movies')
-    // }
+    getAll(){
+        return axios.get('movies')
+    }
 }
 
 export const movies = new Movies();
